c5cb24b1ceda4f5e57fccdd3691e5101f4bc6148
Cxj <1481240653@qq.com>
2020-12-07 21:20:10 +0800

    updata

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  configreWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views'
+      }
+    }
+  }
+}
